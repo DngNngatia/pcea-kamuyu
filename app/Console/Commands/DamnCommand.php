7ae73572c5ee637e0fcd44d94352dc0a3d98fc6c
Derrick <derrick@solutech.co.ke>
2019-11-26 10:13:37 +0300

    damn command

---
+++ app/Console/Commands/DamnCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use ExponentPhpSDK\Exceptions\ExpoException;
+use ExponentPhpSDK\Expo;
+use Illuminate\Console\Command;
+
+class DamnCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fuck:You';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::get();
+        foreach ($users as $user) {
+            try{
+                $expo = Expo::normalSetup();
+                $notification = ['body' => 'fuck u uploaded a new quote', 'sound' => 'default'];
+                $expo->notify((string)$user->id, $notification);
+            }catch (ExpoException $e){
+                $expo = Expo::normalSetup();
+                $expo->subscribe($user->id, $user->device_token);
+                $notification = ['body' => 'fuck u uploaded a new quote', 'sound' => 'default'];
+                $expo->notify((string)$user->id, $notification);
+            }
+
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DamnCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        DamnCommand::class,
     ];
 
     /**
@@ -24,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('fuck:you')->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
