08def2e98bd2bf2b44eb3cedf7b3c8d958592170
Derrick <derrick@solutech.co.ke>
2020-12-12 19:17:07 +0300

    nova church app changes

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use App\Data\Models\Church;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -20,6 +21,15 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name', 'last_name', 'email', 'phone_number', 'church_id', 'device_token', 'password',
     ];
+    protected static function boot()
+    {
+        parent::boot();
+
+        // auto-sets values on creation
+        static::creating(function (User $user) {
+            $user->church_id = Auth::user()->church_id;
+        });
+    }
 
     /**
      * The attributes that should be hidden for arrays.
